5c134ddb8caf1bc59db6ada20d6ee9f3a26ba10f
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2024-11-30 02:32:21 +0900

    feat: setup plop

---
+++ plopfile.mjs
@@ -0,0 +1,87 @@
+import fs from 'fs';
+
+const features = fs.existsSync('src/features') ? fs.readdirSync('src/features', { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name) : [];
+
+export default function (
+    /** @type {import('plop').NodePlopAPI} */
+    plop
+) {
+    plop.setGenerator("component", {
+        description: "Generate a new React component", // 説明文
+        prompts: [
+            // プロンプト
+            {
+                type: 'list',
+                name: 'dir',
+                message: 'Which directory?',
+                choices: ['components', 'features'],
+            },
+            {
+                type: 'list',
+                name: 'featureName',
+                message: 'Feature name:',
+                choices: features.length ?  [...features, { name: 'new feature(create new feature)', value: '' }] : [{ name: 'new feature(create new feature)', value: '' }], // フィーチャーがない場合はNoneを選択
+                when: function (answers) {
+                    return answers.dir === 'features';
+                }
+            },
+            {
+                type: 'input',
+                name: 'newFeatureName',
+                message: 'new feature dir name:',
+                validate: function (value) {
+                    // dir 名が空の場合はエラー
+                    if (/.+/.test(value)) {
+                        return true;
+                    }
+                    return "new feature dir name is required";
+                },
+                when: function (answers) {
+                    return answers.featureName === '';
+                }
+            },
+            {
+                type: 'input',
+                name: 'componentName',
+                message: 'Component name:',
+                validate: function (value) {
+                    // コンポーネント名が空の場合はエラー
+                    if (/.+/.test(value)) {
+                        return true;
+                    }
+                    return "Component name is required";
+                },
+            },
+
+        ],
+        actions: function (data) {
+            const actions = [];
+            if(data.dir === 'features' && data.featureName === '') {
+                actions.push({
+                    type: 'add',
+                    path: 'src/features/{{newFeatureName}}/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx',
+                    templateFile: './plop-templates/components/Component.tsx.hbs'
+                })
+            }
+            if(data.dir === 'features' && data.featureName !== '') {
+                actions.push({
+                    type: 'add',
+                    path: 'src/features/{{featureName}}/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx',
+                    templateFile: './plop-templates/components/Component.tsx.hbs'
+                })
+            }
+            if(data.dir === 'components') {
+                actions.push({
+                    type: 'add',
+                    path: 'src/components/{{pascalCase componentName}}/{{pascalCase componentName}}.tsx',
+                    templateFile: './plop-templates/components/Component.tsx.hbs'
+                })
+            }
+
+            return actions;
+        }
+
+    });
+}
